da1f97a670017afb6b8c07d8a797ec64e50fec0d
Tonyqwe <143794607+Tonyqweq@users.noreply.github.com>
2026-03-26 21:33:02 +0800

    new

--- src/main/java/org/tonyqwe/cinemaweb/mapper/MovieMapper.java
+++ src/main/java/org/tonyqwe/cinemaweb/mapper/MovieMapper.java
@@ -2,9 +2,14 @@ package org.tonyqwe.cinemaweb.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import org.tonyqwe.cinemaweb.domain.entity.Movie;
+import org.tonyqwe.cinemaweb.domain.entity.SysUsers;
 
 @Mapper
 public interface MovieMapper extends BaseMapper<Movie> {
+    @Select("SELECT * FROM movie WHERE title = #{title}")
+    SysUsers selectByTitle(@Param("title") String title);
 }
 
