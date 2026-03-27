72e702ed30b381c0f00477f21c9fee428ece5be4
Tonyqwe <143794607+Tonyqweq@users.noreply.github.com>
2026-03-27 22:53:33 +0800

    new features

--- src/main/java/org/tonyqwe/cinemaweb/mapper/MovieMapper.java
+++ src/main/java/org/tonyqwe/cinemaweb/mapper/MovieMapper.java
@@ -2,14 +2,15 @@ package org.tonyqwe.cinemaweb.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.tonyqwe.cinemaweb.domain.entity.Movie;
-import org.tonyqwe.cinemaweb.domain.entity.SysUsers;
 
 @Mapper
 public interface MovieMapper extends BaseMapper<Movie> {
-    @Select("SELECT * FROM movie WHERE title = #{title}")
-    SysUsers selectByTitle(@Param("title") String title);
+    @Select("SELECT DISTINCT language FROM movie WHERE language IS NOT NULL AND language <> '' ORDER BY language")
+    java.util.List<String> selectDistinctLanguages();
+
+    @Select("SELECT DISTINCT country FROM movie WHERE country IS NOT NULL AND country <> '' ORDER BY country")
+    java.util.List<String> selectDistinctCountries();
 }
 
