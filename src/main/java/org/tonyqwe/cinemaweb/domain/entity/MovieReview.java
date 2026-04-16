bd2ea5d152a367d9f88cbd3e32ea498655505804
Tonyqwe <143794607+Tonyqweq@users.noreply.github.com>
2026-04-16 20:45:41 +0800

    1

--- src/main/java/org/tonyqwe/cinemaweb/domain/entity/MovieReview.java
+++
@@ -1,9 +0,0 @@
-package org.tonyqwe.cinemaweb.domain.entity;
-
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableName;
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Data;
