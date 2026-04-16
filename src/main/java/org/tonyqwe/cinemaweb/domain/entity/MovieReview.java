b498a5335c8c5c8c53fd3f73c2a47dba66bc2484
Tonyqwe <143794607+Tonyqweq@users.noreply.github.com>
2026-04-16 20:45:26 +0800

    1

--- src/main/java/org/tonyqwe/cinemaweb/domain/entity/MovieReview.java
+++ src/main/java/org/tonyqwe/cinemaweb/domain/entity/MovieReview.java
@@ -7,35 +7,3 @@ import com.baomidou.mybatisplus.annotation.TableName;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
-
-import java.util.Date;
-
-@Data
-@TableName("movie_reviews")
-public class MovieReview {
-
-    @TableId(type = IdType.AUTO)
-    private Long id;
-
-    @JsonProperty("movie_id")
-    @TableField("movie_id")
-    private Long movieId;
-
-    @JsonProperty("user_id")
-    @TableField("user_id")
-    private Integer userId;
-
-    private Integer rating;
-
-    private String comment;
-
-    @JsonProperty("created_at")
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Shanghai")
-    @TableField("created_at")
-    private Date createdAt;
-
-    @JsonProperty("updated_at")
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Shanghai")
-    @TableField("updated_at")
-    private Date updatedAt;
-}
